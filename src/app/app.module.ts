73189dee99a0b464fb4057bf1f2e086393303077
Denise Risia <risiaresponde@gmail.com>
2023-07-20 09:39:58 -0300

    add router and module/componet main and css for main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeSiteModule } from './home-site/home-site.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HomeSiteModule,
+    FormsModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
