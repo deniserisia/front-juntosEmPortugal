1cce817143ab8490ceb14ddd852ca80959134701
Denise Risia <risiaresponde@gmail.com>
2023-07-20 10:08:31 -0300

    add module for footer and header full site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeSiteModule } from './home-site/home-site.module';
 import { FormsModule } from '@angular/forms';
+import { TemplateModule } from './template/template.module';
+import { HeaderComponent } from './template/header/header.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HomeSiteModule,
     FormsModule,
+    TemplateModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
